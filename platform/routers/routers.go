0c727e93903e2c91a5bdd984cad7b68dafa3e3b0
Adi12-lab <adiiskandarubaidah@gmail.com>
2024-11-10 20:25:56 +0700

    feat: cors for development

--- platform/routers/routers.go
+++ platform/routers/routers.go
@@ -66,7 +66,18 @@ func (r *routing) Serve() {
 
 // CORSHandler handles requests with unsupported HTTP methods.
 func CORSHandler(c *gin.Context) {
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	origin := c.Request.Header.Get("Origin")
+	if origin != "" {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+	}
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+	if c.Request.Method == http.MethodOptions {
+		c.AbortWithStatus(http.StatusNoContent)
+		return
+	}
 	c.Next()
 }
 
